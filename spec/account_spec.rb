b6b3fd5de4a02b385ad94be4715a8afe52b074a1
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-01-27 23:28:01 +0000

    refactor: update account unit test to make sure it is correctly de-coupled

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,8 @@
 require "account"
 
 describe Account do
-  let(:account) { Account.new }
+  let(:statement) { double(:statement, add_transaction: nil, format_transactions: "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00") }
+  let(:account) { Account.new(statement) }
 
   describe "#deposit" do
     it "should return 1 after a deposit of 1" do
@@ -17,6 +18,13 @@ describe Account do
 
     it "should raise an error if the user tries to withdraw more money than the actual balance" do
       expect { account.withdraw(1) }.to raise_error "You don't have enough balance"
+    end
+  end
+
+  describe "#print_statement" do
+    it "should return a nice formatted table with date, credit, debit and balance" do
+      account.deposit(1000)
+      expect(account.print_statement).to eq "date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00"
     end
   end
 end
